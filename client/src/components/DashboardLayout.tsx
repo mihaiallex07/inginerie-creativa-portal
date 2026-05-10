fddf53f092fad800ad66c2885de964d0a03661ff
Mihai Porumboiu <mihai.porumboiu@ingineriecreativa.ro>
2026-05-10 21:57:08 +0300

    Fix implicit any in DashboardLayout

--- client/src/components/DashboardLayout.tsx
+++ client/src/components/DashboardLayout.tsx
@@ -319,7 +319,7 @@ function DashboardLayoutContent({
 
   const initials = user?.name
     ?.split(" ")
-    .map((n) => n[0])
+    .map((n: string) => n[0])
     .slice(0, 2)
     .join("")
     .toUpperCase() ?? "?";
